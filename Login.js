49b3558565ff91299fbcc01b58dceaa9e072ca15
poteramalcolm@gmail.com <104031265+mal-prog@users.noreply.github.com>
2022-06-09 13:50:13 -0600

    connecting login to main page

---
+++ Login.js
@@ -0,0 +1,40 @@
+import { useState} from "react";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+
+const Login = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [oneTimePassword, setOneTimePassword] = useState(null);
+
+  return ( 
+    <SafeAreaView style={styles.mainView}>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPhoneNumber}
+        value={phoneNumber}
+        placeholder="801-555-1212"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setOneTimePassword}
+        value={oneTimePassword}
+        placeholder="1234"
+        keyboardType="numeric"
+        secureTextEntry={true}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  mainView:{
+     marginTop:100
+  }
+});
+
+export default Login
